86b8e8fe0358a853e5aeb145ff7cd61282130ede
williamd4112 <williamd4112@hotmail.com>
2017-10-04 01:21:18 +0800

    remove useless file

--- Sim/Scripts/SocketRawDataListener.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-abstract public class SocketRawDataListener : MonoBehaviour {
-    abstract public void OnReceiveRawData(ref byte[] data);
-}
--- Sim/Scripts/TerminationProducer.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-abstract public class TerminationProducer : MonoBehaviour {
-    abstract public byte[] GetTermination();
-    abstract public int GetTerminationSize();
-}
