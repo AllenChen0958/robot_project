ecc7ccacc1fa51f6e7a09f14bbdb5b652f62563a
AllenChen0958 <allenchen0958@gmail.com>
2017-10-04 00:15:13 +0800

    upload_unity_scripts

---
+++ Sim/Scripts/Reward/DistanceRewardProducer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GymUnity3D
+{
+    public class DistanceRewardProducer : RewardProducer
+    {
+        [SerializeField]
+        private Transform m_Target;
+
+        [SerializeField]
+        private Transform m_Source;
+
+        public override float GetReward()
+        {
+            float dis = Vector3.Distance(m_Source.position, m_Target.position);
+            return dis;
+        }
+
+        public override byte[] GetState()
+        {
+            float r = GetReward();
+            return BitConverter.GetBytes(r);
+        }
+
+        public override int GetStateSize()
+        {
+            return sizeof(float);
+        }
+
+        public override void Reset()
+        {
+         
+        }
+    }
+
+}
