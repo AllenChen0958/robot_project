e57666952c6dac7f6b3b8542b45ba807c00d6060
AllenChen0958 <allenchen0958@gmail.com>
2017-10-04 00:02:31 +0800

    segmentation

---
+++ trashe725/image-analysis-package/Script/SegmentationScript.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SegmentationScript : MonoBehaviour {
+
+	[HideInInspector]
+	public Shader shader=null;
+
+	private Camera maincam;
+	private Camera dummycam;
+	public RenderTexture target=null;
+	public bool enable = true;
+
+
+
+
+	void CreateDummyCamera(){
+		GameObject dummy = new GameObject ();
+		dummy.name = "HiddenSegmentationCamera";
+		dummy.transform.SetParent (this.transform);
+		dummy.transform.localPosition = Vector3.zero;
+		dummy.transform.localRotation = Quaternion.identity;
+		dummy.transform.localScale = Vector3.one;
+
+		//dummy.hideFlags = HideFlags.HideInHierarchy;
+
+		dummycam = dummy.AddComponent<Camera> ();
+
+		dummycam.cullingMask = maincam.cullingMask;
+		dummycam.aspect = maincam.aspect;
+		dummycam.nearClipPlane = maincam.nearClipPlane;
+		dummycam.farClipPlane = maincam.farClipPlane;
+		dummycam.fieldOfView = maincam.fieldOfView;
+		dummycam.targetTexture = target;
+		dummycam.rect = maincam.rect;
+		dummycam.depth = maincam.depth+1;
+		dummycam.clearFlags = CameraClearFlags.Color;
+		dummycam.backgroundColor = Color.black;
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		maincam = this.GetComponent<Camera> ();
+
+		if(shader == null)
+			shader = Shader.Find ("Hidden/SegmentationShader");
+
+		CreateDummyCamera ();
+
+		dummycam.depthTextureMode = DepthTextureMode.DepthNormals;
+		dummycam.SetReplacementShader (shader, "");
+
+		UpdateMaterialPropertyBlock ();
+	}
+
+	void UpdateMaterialPropertyBlock(){
+		var renderers = GameObject.FindObjectsOfType<Renderer> ();
+		var prob = new MaterialPropertyBlock ();
+
+		foreach (var r in renderers) {
+			var tag = r.gameObject.tag;
+			prob.SetColor ("_ObjectColor", TagsManager.GetColor (tag));
+			r.SetPropertyBlock (prob);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		dummycam.enabled = enable;
+		if(enable)
+			UpdateMaterialPropertyBlock ();
+	}
+
+	public Camera GetDummyCamera(){
+		return dummycam;
+	}
+
+	public RenderTexture TargetTexture{
+		set{ 
+			target = value;
+			dummycam.targetTexture = target;
+		}
+		get{ return target; }
+	}
+
+	public void Render(){
+		dummycam.Render ();
+	}
+}
---
+++ trashe725/image-analysis-package/Shader/SegmentationShader.shader
@@ -0,0 +1,63 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+
+
+Shader "Hidden/SegmentationShader" {
+
+	Properties {
+		_MainTex ("", 2D) = "white" {}
+		_CutOff ("", Float) = 0.3
+		_ObjectColor("", Color) = (0, 0, 0, 1)
+	}
+
+	CGINCLUDE
+
+	#include "UnityCG.cginc"
+
+	uniform fixed4 _ObjectColor;
+	uniform sampler2D _MainTex;
+	uniform float4 _MainTex_ST;
+	uniform fixed _Cutoff;
+
+	struct input{
+		float4 vertex : POSITION;
+		float4 texcoord : TEXCOORD0;
+	};
+
+	struct v2f {
+	    float4 pos : SV_POSITION;
+	    float2 uv : TEXCOORD0;
+	};
+
+	v2f vert( input v ) {
+	    v2f o;
+	    o.pos = UnityObjectToClipPos(v.vertex);
+	    o.uv = TRANSFORM_TEX(v.texcoord, _MainTex);
+	    return o;
+	}
+
+	fixed4 frag(v2f i) : SV_Target {
+		fixed4 tex = tex2D( _MainTex, i.uv);
+		if(tex.a < _Cutoff)
+			discard;
+		//clip( tex.a - _Cutoff );
+		return _ObjectColor;
+	}
+
+	ENDCG
+
+	SubShader{
+		Tags { }
+		Pass {
+
+			CGPROGRAM
+			#pragma target 3.0
+			#pragma vertex vert
+			#pragma fragment frag
+
+			ENDCG
+		}
+	}
+
+	FallBack "Hidden/InternalErrorShader"
+}
